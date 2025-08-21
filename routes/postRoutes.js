2c0890f661db85129dd644476950cc44654205d6
baticlean <baticlean225@gmail.com>
2025-08-21 20:20:07 +0000

    ajout

---
+++ routes/postRoutes.js
@@ -0,0 +1,14 @@
+// routes/postRoutes.js
+const express = require('express');
+const router = express.Router();
+const { createPost, getPosts } = require('../controllers/postController');
+const { protect } = require('../middleware/authMiddleware');
+
+// Route pour récupérer tous les posts (publique)
+router.get('/', getPosts);
+
+// Route pour créer un post (protégée)
+// Le middleware `protect` s'exécutera avant `createPost`
+router.post('/', protect, createPost);
+
+module.exports = router;s
